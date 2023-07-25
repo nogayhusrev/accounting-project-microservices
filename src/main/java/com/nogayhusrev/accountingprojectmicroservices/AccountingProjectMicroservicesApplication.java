62188d560adcd107980bd55cb82567b3a44eb7b3
nogayhusrev <nogayhusrev@gmail.com>
2023-07-25 12:41:32 +0300

    Initial Commit

---
+++ src/main/java/com/nogayhusrev/accountingprojectmicroservices/AccountingProjectMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.nogayhusrev.accountingprojectmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingProjectMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingProjectMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nogayhusrev/accountingprojectmicroservices/AccountingProjectMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nogayhusrev.accountingprojectmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingProjectMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
